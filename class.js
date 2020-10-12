b8f62f7933fe8c7da8f261c61cc7f3aefc129e02
unknown <locadmn@SP-02716938.college.concordia.sa.edu.au>
2020-10-12 20:02:15 +1030

    new project(platformer) commit

--- class.js
+++ class.js
@@ -1,89 +1,34 @@
+class Tile{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.size = 25
 
-class player{
-    constructor(){
-        this.lane = 1
-        this.radius = 60
-        this.laneList = [100,300,500]
-        this.y = height - 100
-        this.x = 0
-        this.phase = "white"
-        this.reinforce = false
+    }
+    render(){
+        fill("white")
+        rect(this.x,this.y,this.size)
+    }
+}
+
+
+
+class Player{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.width = 40
+        this.height = 60
+        this.speed = 8
+
     }
     move(){
-        // sets x-coord based off lane array
-        this.x = this.laneList[this.lane]
-    }
-
-    display(){
-        fill("black")
-        stroke("grey")
-        if(this.reinforce == true){
-            stroke("cyan")
-        }
-        strokeWeight(2)
-        ellipse(this.x,this.y,this.radius)
-        strokeWeight(0)
-        fill(this.phase)
-        ellipse(this.x,this.y,this.radius/3)
-    }
-}
-
-
-
-
-
-class wall{
-    constructor(x,phase){
-        this.group
-        this.lane = int(random(0,3))
-        this.width = (width/3)+6
-        this.height = 30
-        this.x = x
-        this.y = 0
-        this.phase = phase
-        this.death = false
-    }
-
-    display(){
-        strokeWeight(0)
-        fill(this.phase)
-        rect(this.x,this.y,this.width,this.height)
-    }
-
-    move(speed){
-        this.y += speed
-        if (this.y > height){
-            this.death = true
-
-        }
-    }
-}
-
-
-
-class powerUp{
-    constructor(roll,lane){
-        this.radius = 30
-        this.type = roll
-        if (roll < 6){
-            this.border = "gold"
-        } else {this.border = "red"}
-        this.laneList = [100,300,500]
-        this.x = lane + 100
-        this.y = 25
-        this.death = false
+        if (keyIsDown(68)){this.x += this.speed}
+        if (keyIsDown(65)){this.x -= this.speed}
     }
     display(){
-        strokeWeight(1)
-        stroke(this.border)
+        
         fill("white")
-        ellipse(this.x,this.y,this.radius)
-    }
-    move(speed){
-        this.y += speed
-        if (this.y > height){
-            this.death = true
-
-        }
+        rect(this.x,this.y,this.width,this.height)
     }
 }
---
+++ phase_shift/class.js
@@ -0,0 +1,89 @@
+
+class Player{
+    constructor(){
+        this.lane = 1
+        this.radius = 60
+        this.laneList = [100,300,500]
+        this.y = height - 100
+        this.x = 0
+        this.phase = "white"
+        this.reinforce = false
+    }
+    move(){
+        // sets x-coord based off lane array
+        this.x = this.laneList[this.lane]
+    }
+
+    display(){
+        fill("black")
+        stroke("grey")
+        if(this.reinforce == true){
+            stroke("cyan")
+        }
+        strokeWeight(2)
+        ellipse(this.x,this.y,this.radius)
+        strokeWeight(0)
+        fill(this.phase)
+        ellipse(this.x,this.y,this.radius/3)
+    }
+}
+
+
+
+
+
+class Wall{
+    constructor(x,phase){
+        this.group
+        this.lane = int(random(0,3))
+        this.width = (width/3)+6
+        this.height = 30
+        this.x = x
+        this.y = 0
+        this.phase = phase
+        this.death = false
+    }
+
+    display(){
+        strokeWeight(0)
+        fill(this.phase)
+        rect(this.x,this.y,this.width,this.height)
+    }
+
+    move(speed){
+        this.y += speed
+        if (this.y > height){
+            this.death = true
+
+        }
+    }
+}
+
+
+
+class PowerUp{
+    constructor(roll,lane){
+        this.radius = 30
+        this.type = roll
+        if (roll < 6){
+            this.border = "gold"
+        } else {this.border = "red"}
+        this.laneList = [100,300,500]
+        this.x = lane + 100
+        this.y = 25
+        this.death = false
+    }
+    display(){
+        strokeWeight(1)
+        stroke(this.border)
+        fill("white")
+        ellipse(this.x,this.y,this.radius)
+    }
+    move(speed){
+        this.y += speed
+        if (this.y > height){
+            this.death = true
+
+        }
+    }
+}
